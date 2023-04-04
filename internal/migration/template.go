fa14dc18f7fc9fc3e0efa5a70db40657cc582503
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-04-04 03:12:36 +0100

    remove redundant comments from template

--- internal/migration/template.go
+++ internal/migration/template.go
@@ -9,11 +9,4 @@ const downMigrationFileTemplate = `-- Undo the changes made in the up migration
 const upMigrationFileTemplate = `-- Perform migration here.
 --
 --  * Make migrations idempotent (use IF EXISTS)
---  * Make migrations backwards-compatible (old readers/writers must continue to work)
---  * If you are using CREATE INDEX CONCURRENTLY, then make sure that only one statement
---    is defined per file, and that each such statement is NOT wrapped in a transaction.
---    Each such migration must also declare "createIndexConcurrently: true" in their
---    associated metadata.yaml file.
---  * If you are modifying Postgres extensions, you must also declare "privileged: true"
---    in the associated metadata.yaml file.
 `
