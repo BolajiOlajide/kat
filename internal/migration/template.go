8b0150b5a9eec0cdd202e83c602765d50050cc6f
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2025-04-23 02:18:46 +0100

    include note about transactions in kat

--- internal/migration/template.go
+++ internal/migration/template.go
@@ -7,10 +7,16 @@ description: |
 `
 
 const downMigrationFileTemplate = `-- Undo the changes made in the up migration
+--
+-- Note: All migrations in kat are automatically wrapped in a transaction.
+-- You don't need to add BEGIN/COMMIT statements manually.
 `
 
 const upMigrationFileTemplate = `-- Perform migration here.
 --
 --  It's helpful to make migrations idempotent, that way migrations can be executed multiple times
 -- and the database structure will be the same.
+--
+-- Note: All migrations in kat are automatically wrapped in a transaction.
+-- You don't need to add BEGIN/COMMIT statements manually.
 `
