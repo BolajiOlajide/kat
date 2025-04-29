1afa822b22b450beaf5c3326f191e6bc381bbd70
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2025-04-29 02:46:11 +0100

    make use of proper constants

--- internal/migration/migration.go
+++ internal/migration/migration.go
@@ -9,6 +9,9 @@ import (
 	"github.com/BolajiOlajide/kat/internal/types"
 )
 
+// FilePerm is the standard permission for migration files (readable by all, writable by owner)
+const FilePerm = 0644
+
 func saveMigration(m types.Migration, name string) (err error) {
 	defer func() {
 		if err != nil {
@@ -20,27 +23,27 @@ func saveMigration(m types.Migration, name string) (err error) {
 	}()
 
 	// write the up.sql file
-	if err := os.MkdirAll(filepath.Dir(m.Up), 0755); err != nil {
+	if err := os.MkdirAll(filepath.Dir(m.Up), os.ModePerm); err != nil {
 		return err
 	}
-	if err := os.WriteFile(m.Up, []byte(upMigrationFileTemplate), os.FileMode(0644)); err != nil {
+	if err := os.WriteFile(m.Up, []byte(upMigrationFileTemplate), os.FileMode(FilePerm)); err != nil {
 		return err
 	}
 
 	// write the down.sql file
-	if err := os.MkdirAll(filepath.Dir(m.Down), 0755); err != nil {
+	if err := os.MkdirAll(filepath.Dir(m.Down), os.ModePerm); err != nil {
 		return err
 	}
-	if err := os.WriteFile(m.Down, []byte(downMigrationFileTemplate), os.FileMode(0644)); err != nil {
+	if err := os.WriteFile(m.Down, []byte(downMigrationFileTemplate), os.FileMode(FilePerm)); err != nil {
 		return err
 	}
 
 	// write the metadata.yaml file
-	if err := os.MkdirAll(filepath.Dir(m.Metadata), 0755); err != nil {
+	if err := os.MkdirAll(filepath.Dir(m.Metadata), os.ModePerm); err != nil {
 		return err
 	}
 	metadata := fmt.Sprintf(metadataFileTemplate, name, m.Timestamp)
-	if err := os.WriteFile(m.Metadata, []byte(metadata), os.FileMode(0644)); err != nil {
+	if err := os.WriteFile(m.Metadata, []byte(metadata), os.FileMode(FilePerm)); err != nil {
 		return err
 	}
 
