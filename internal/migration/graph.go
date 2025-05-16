df07174d0114fe1f73201b555b539765c80343d2
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2025-05-16 04:15:44 +0200

    revert indentation options in editorconfig

--- internal/migration/graph.go
+++ internal/migration/graph.go
@@ -1,16 +1,16 @@
 package migration
 
 import (
-  "github.com/cockroachdb/errors"
-  "io/fs"
+	"github.com/cockroachdb/errors"
+	"io/fs"
 
-  "github.com/BolajiOlajide/kat/internal/types"
+	"github.com/BolajiOlajide/kat/internal/types"
 
-  "github.com/dominikbraun/graph"
+	"github.com/dominikbraun/graph"
 )
 
 var definitionHash = func(d types.Definition) int64 {
-  return d.Timestamp
+	return d.Timestamp
 }
 
 // ComputeDefinitions builds a directed acyclic graph (DAG) from migration definitions.
@@ -21,50 +21,50 @@ var definitionHash = func(d types.Definition) int64 {
 // The function expects directories to be timestamp-prefixed following Kat's convention.
 // It returns the constructed graph and any errors encountered during graph construction.
 func ComputeDefinitions(f fs.FS) (graph.Graph[int64, types.Definition], error) {
-  // create a DAG (Directed Acyclic graph) to represent a migration. This makes it easy
-  // to compute execution order for migrations and also to export the graph as a digraph
-  // diagram.
-  // You can visualize using an online tool like:
-  // https://dreampuf.github.io/GraphvizOnline
-  g := graph.New(definitionHash, graph.Acyclic(), graph.Directed())
+	// create a DAG (Directed Acyclic graph) to represent a migration. This makes it easy
+	// to compute execution order for migrations and also to export the graph as a digraph
+	// diagram.
+	// You can visualize using an online tool like:
+	// https://dreampuf.github.io/GraphvizOnline
+	g := graph.New(definitionHash, graph.Acyclic(), graph.Directed())
 
-  mf, err := extractMigrationFiles(f)
-  if err != nil {
-    return nil, err
-  }
+	mf, err := extractMigrationFiles(f)
+	if err != nil {
+		return nil, err
+	}
 
-  // Because Kat prepends it's directories with a timestamp, we expect the migrations
-  // to be automatically sorted, so we can pretty much assume the loop is going to be
-  // in ascending order from the oldest migrations to newest.
-  //
-  // If this isn't the case, adding an edge will result in an error, because the parent
-  // edge won't be found in the graph.
-  for _, file := range mf {
-    if !file.IsDir() {
-      // if this is not a directory, skip it.
+	// Because Kat prepends it's directories with a timestamp, we expect the migrations
+	// to be automatically sorted, so we can pretty much assume the loop is going to be
+	// in ascending order from the oldest migrations to newest.
+	//
+	// If this isn't the case, adding an edge will result in an error, because the parent
+	// edge won't be found in the graph.
+	for _, file := range mf {
+		if !file.IsDir() {
+			// if this is not a directory, skip it.
 
-      // Kat expects migrations to live in directories, so this is one of the most
-      // important validations we can have
-      continue
-    }
+			// Kat expects migrations to live in directories, so this is one of the most
+			// important validations we can have
+			continue
+		}
 
-    definition, err := computeDefinition(f, file.Name())
-    if err != nil {
-      return nil, errors.Wrap(err, "malformed migration definition")
-    }
+		definition, err := computeDefinition(f, file.Name())
+		if err != nil {
+			return nil, errors.Wrap(err, "malformed migration definition")
+		}
 
-    // We add each migration as a vertex to the graph
-    if err := g.AddVertex(definition); err != nil {
-      return nil, errors.Wrap(err, "error adding vertex")
-    }
+		// We add each migration as a vertex to the graph
+		if err := g.AddVertex(definition); err != nil {
+			return nil, errors.Wrap(err, "error adding vertex")
+		}
 
-    // Then we define the relationship with its parent by adding its edges.
-    for _, parent := range definition.Parents {
-      if err := g.AddEdge(parent, definition.Timestamp); err != nil {
-        return nil, errors.Wrapf(err, "error adding edge for parent: %d", parent)
-      }
-    }
-  }
+		// Then we define the relationship with its parent by adding its edges.
+		for _, parent := range definition.Parents {
+			if err := g.AddEdge(parent, definition.Timestamp); err != nil {
+				return nil, errors.Wrapf(err, "error adding edge for parent: %d", parent)
+			}
+		}
+	}
 
-  return g, nil
+	return g, nil
 }
--- internal/migration/up.go
+++ internal/migration/up.go
@@ -1,56 +1,56 @@
 package migration
 
 import (
-  "context"
-  "github.com/dominikbraun/graph"
+    "context"
+    "github.com/dominikbraun/graph"
 
-  "github.com/cockroachdb/errors"
-  "github.com/urfave/cli/v2"
+    "github.com/cockroachdb/errors"
+    "github.com/urfave/cli/v2"
 
-  "github.com/BolajiOlajide/kat/internal/database"
-  "github.com/BolajiOlajide/kat/internal/runner"
-  "github.com/BolajiOlajide/kat/internal/types"
+    "github.com/BolajiOlajide/kat/internal/database"
+    "github.com/BolajiOlajide/kat/internal/runner"
+    "github.com/BolajiOlajide/kat/internal/types"
 )
 
 // Up is the command that runs the up migration operation.
 func Up(c *cli.Context, cfg types.Config, dryRun bool) error {
-  f, err := getMigrationsFS(cfg.Migration.Directory)
-  if err != nil {
-    return err
-  }
+    f, err := getMigrationsFS(cfg.Migration.Directory)
+    if err != nil {
+        return err
+    }
 
-  definitions, err := ComputeDefinitions(f)
-  if err != nil {
-    return err
-  }
+    definitions, err := ComputeDefinitions(f)
+    if err != nil {
+        return err
+    }
 
-  dbConn, err := cfg.Database.ConnString()
-  if err != nil {
-    return err
-  }
+    dbConn, err := cfg.Database.ConnString()
+    if err != nil {
+        return err
+    }
 
-  db, err := database.New(dbConn)
-  if err != nil {
-    return err
-  }
+    db, err := database.New(dbConn)
+    if err != nil {
+        return err
+    }
 
-  return UpWithFS(c.Context, db, definitions, cfg, dryRun)
+    return UpWithFS(c.Context, db, definitions, cfg, dryRun)
 }
 
 func UpWithFS(ctx context.Context, db database.DB, definitions graph.Graph[int64, types.Definition], cfg types.Config, dryRun bool) error {
-  defer db.Close()
+    defer db.Close()
 
-  // No retry for migrations, just basic connection
-  r, err := runner.NewRunner(ctx, db)
-  if err != nil {
-    return errors.Wrap(err, "connecting to database")
-  }
+    // No retry for migrations, just basic connection
+    r, err := runner.NewRunner(ctx, db)
+    if err != nil {
+        return errors.Wrap(err, "connecting to database")
+    }
 
-  return r.Run(ctx, runner.Options{
-    Operation:     types.UpMigrationOperation,
-    Definitions:   definitions,
-    MigrationInfo: cfg.Migration,
-    DryRun:        dryRun,
-    Verbose:       cfg.Verbose,
-  })
+    return r.Run(ctx, runner.Options{
+        Operation:     types.UpMigrationOperation,
+        Definitions:   definitions,
+        MigrationInfo: cfg.Migration,
+        DryRun:        dryRun,
+        Verbose:       cfg.Verbose,
+    })
 }
